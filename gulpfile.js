9cc079ccee3bc6ca7cb2cdff2382403e5cad801b
wuming <wuming@maihaoche.com>
2018-02-09 20:46:36 +0800

    gulpfile修改

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,29 @@
-var gulp = require('gulp');
-var rename = require("gulp-rename");
-var minifyCSS = require('gulp-minify-css');
-var uglify = require("gulp-uglify");
-var autoprefixer=require("gulp-autoprefixer");
+const gulp = require('gulp');
+const postcss=require('gulp-postcss');
+const rename = require("gulp-rename");
+const minifyCSS = require('gulp-minify-css');
+const uglify = require("gulp-uglify");
+const sass=require("gulp-sass");
 gulp.task('export-css', function () {
-    return gulp.src('./src/css/Jcalendar.css')
-        .pipe(autoprefixer({
-            browsers:["Android>=4.0","iOS>=7"]
-        }))
+    return gulp.src('./src/scss/Jcalendar.scss')
+        .pipe(sass())
+        .pipe(postcss())
+        .pipe(gulp.dest('./lib/css'))       
         .pipe(minifyCSS())
         .pipe(rename({suffix:'.min'}))
-        .pipe(gulp.dest('./lib'));
+        .pipe(gulp.dest('./lib/css'));
 });
 gulp.task('export-js',function(){
     return gulp.src('./src/js/Jcalendar.js')
+           .pipe(gulp.dest('./lib/js'))
            .pipe(uglify())
            .pipe(rename({suffix:'.min'}))
-           .pipe(gulp.dest('./lib'));
+           .pipe(gulp.dest('./lib/js'));
 });   
+
+gulp.task('watch',function(){
+    gulp.watch('./src/scss/Jcalendar.scss',['export-css']);
+    gulp.watch('./src/js/Jcalendar.js',['export-js']);
+});
+
 gulp.task('default', ['export-css','export-js']);
---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    plugins: [
+      require('autoprefixer')({ browsers: ['Android>=4.0','iOS>=7'] })
+    ]
+  }
